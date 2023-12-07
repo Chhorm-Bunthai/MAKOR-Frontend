ec27e4c3a018b42a14031b65b7be9ef191611128
RelexSun <88622289+RelexSun@users.noreply.github.com>
2023-12-08 02:16:44 +0700

    feat: routing sign up page and login page

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,19 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Signup from "./pages/signup/SignUp";
-import "./App.css";
 import LogIn from "./pages/login/login";
-import Main from "./pages/mainpage/Main"
+import Main from "./pages/mainpage/Main";
+import "./App.css";
 function App() {
   return (
-    <>
-    <Main/>
-      <Signup />
-      <LogIn />
-    </>
+    <Router>
+      <>
+        <Main />
+        <Routes>
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/login" element={<LogIn />} />
+        </Routes>
+      </>
+    </Router>
   );
 }
 export default App;
