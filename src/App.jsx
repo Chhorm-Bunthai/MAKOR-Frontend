d07434106c5ca044f618042976c925bcdb79c277
RelexSun <relexsunnop@gmail.com>
2023-12-12 09:37:48 +0700

    remove queryMe function

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { useEffect } from "react";
+// import { useEffect } from "react";
 import Signup from "./pages/signup/SignUp";
 import LogIn from "./pages/login/login";
 import Main from "./pages/mainpage/Main";
@@ -10,22 +10,22 @@ import ResetPassword from "./pages/login/ResetPassword";
 import Cookies from "js-cookie";
 
 import "./App.css";
-import useAuthContext from "./hooks/useAuth";
+// import useAuthContext from "./hooks/useAuth";
 
 function App() {
-  const { Me } = useAuthContext();
+  // const { Me } = useAuthContext();
   // const token = localStorage.getItem("token");
   // console.log("App.js", token);
   const token = Cookies.get("jwt");
 
-  useEffect(() => {
-    const fetchData = async () => {
-      const userData = await Me();
-      console.log(userData);
-    };
+  // useEffect(() => {
+  //   const fetchData = async () => {
+  //     const userData = await Me();
+  //     console.log(userData);
+  //   };
 
-    fetchData();
-  }, []);
+  //   fetchData();
+  // }, []);
   return (
     <BrowserRouter>
       <Navbar />
